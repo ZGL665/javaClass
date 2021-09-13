4d120ac32b4b997abb55634d51905ebede62920a
zgl <zgl.com>
2021-09-13 14:12:51 +0800

    dao设计2

--- src/day9_6/Test1.java
+++ src/day9_6/Test1.java
@@ -1,25 +1,66 @@
 package day9_6;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
-import java.io.IOException;
-import java.util.Properties;
-
-import javax.naming.event.NamespaceChangeListener;
+import com.bwf.dao.IUserdao;
+import com.bwf.dao.impl.UserDao;
+import com.bwf.entity.User;
 
 public class Test1 {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
+
+		IUserdao userDao = new UserDao();
+
+		User user = new User();
+
+		//保存用户
+		/*user.setId(6);
+		user.setName("51testing");
+		user.setSex("女");
+		user.setBirthday(new Date());*/
 
 		/*
-		 * IUserSvevice userSvevice=new UserService1(); userSvevice.show();
+		 * int count=userDao.save(user); if(count==1) {
+		 * 
+		 * System.out.println("用户数据保存成功"); }
 		 */
 
-		Properties pro = new Properties();
-		pro.load(Test1.class.getResourceAsStream("obj.properties"));
+		// 删除用户信息
+		// userDao.del(6);
 
-		//IUserSvevice userSvevice = (IUserSvevice)Class.forName(pro.getProperty("userservice"));
+
+
+		// 更新用户信息
+		/*user.setId(5);
+		user.setName("aaaa");
+		user.setSex("男");
+		user.setBirthday(new Date());
 		
-		//userSvevice.show();
+		userDao.update(user);*/
+
+
+		//查询用户
+		/*User u1=userDao.findById(1);
+		u1=userDao.findById(5);
+		System.out.println(u1.getName());*/
+
+
+		//根据name查询多个用户
+
+		/*List<User> userList1=userDao.findByName("zhang");
+		
+		System.out.println(userList1);*/
+
+
+		//查询所有用户
+
+		List<User> userlist2=userDao.findAll(1);
+
+		System.out.println(userlist2.size());
+
+
 
 	}
-
 }
